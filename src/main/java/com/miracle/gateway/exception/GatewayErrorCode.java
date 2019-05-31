b25fbe0bd42f1a016b44a723bce9f9f610a22e70
MaheshMediboyina <MaheshMediboyina@users.noreply.github.com>
2019-05-31 22:52:50 +0530

    Revert "Updated Logic"
    This reverts commit c9dd95fb427ea29ab4a36d2d489eb71def859370.

--- src/main/java/com/miracle/gateway/exception/GatewayErrorCode.java
+++
@@ -1,6 +0,0 @@
-package com.miracle.gateway.exception;
-
-public interface GatewayErrorCode {
-	String INVALID_FILTER_TYPE = "MSB.GWY.IFT.000";
-	String GATEWAY_CONTROLLER_UNKNOWN_EXCEPTION = "MSB.GWY.UN.000";
-}
