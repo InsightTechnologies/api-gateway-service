c456a0dc67db3049e360b0d95681a49cb276df49
samhithar <samhithaeega@gmail.com>
2019-05-31 15:36:26 -0500

    Updated commit

---
+++ src/main/java/com/miracle/gateway/exception/GatewayErrorCode.java
@@ -0,0 +1,6 @@
+package com.miracle.gateway.exception;
+
+public interface GatewayErrorCode {
+	String INVALID_FILTER_TYPE = "MSB.GWY.IFT.000";
+	String GATEWAY_CONTROLLER_UNKNOWN_EXCEPTION = "MSB.GWY.UN.000";
+}
